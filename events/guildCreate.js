32d54f083dce01651aa4642e690ca18a0f60933a
Hodoo <8972895+Hodoo@users.noreply.github.com>
2019-03-09 11:52:37 -0500

    Fixed issue with joining new servers

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,6 +1,6 @@
 exports.run = (client, guild) => {
   console.log(`${Date.now()}: JOINED: ${guild.name} (${guild.id})`);
-  let server = require("./data/server-default.json");
+  let server = require("../data/server-default.json");
   server.ownerID = guild.ownerID;
   client.settings.set(guild.id, server);
 }
