3e61f7445920ae840695ffa9e7e9ef29e369fe1e
Hodoo <8972895+Hodoo@users.noreply.github.com>
2019-03-12 13:35:18 -0500

    Made automatic updates of server configs more efficient

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,20 @@
 exports.run = (client) => {
+  let currentGuilds = Array.from(client.guilds.keys());
+
+  // Update server settings, if not newest version
+  if (!client.settings.get("lastVersion") || client.settings.get("lastVersion") !== client.version) {
+    console.log(`An update has occured since last launch. All servers' settings will be updated.`)
+    let serverdefault = require("../data/server-default.json");
+    let i;
+    for (i in currentGuilds) {
+      let guild = client.guilds.get(currentGuilds[i]);
+      let server = client.settings.get(currentGuilds[i]);
+      server = Object.assign(serverdefault, server);
+      console.log(`${guild.name} (${guild.id}) settings have been updated to newest version`);
+    }
+    client.settings.set("lastVersion", client.version);
+  }
+
   client.user.setActivity('your votes', { type: 'WATCHING' });
   console.log(`ChaosBot version: ${client.version}`);
   console.log("ChaosBot is running");
