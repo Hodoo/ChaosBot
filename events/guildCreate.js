f1da69b7f7fb200a53e1472171d46c44ecce3519
Hodoo <8972895+Hodoo@users.noreply.github.com>
2019-09-21 13:33:12 -0400

    Added workaround to keep server settings if guildCreate misfires

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,6 +1,11 @@
 exports.run = (client, guild) => {
-  console.log(`${Date.now()}: JOINED: ${guild.name} (${guild.id})`);
-  let server = require("../data/server-default.json");
-  server.ownerID = guild.ownerID;
-  client.settings.set(guild.id, server);
+  if (client.settings.has(guild.id)) {
+    console.log(`${Date.now()}: REJOINED: ${guild.name} (${guild.id})`);
+  }
+  else {
+    console.log(`${Date.now()}: JOINED: ${guild.name} (${guild.id})`);
+    let server = require("../data/server-default.json");
+    server.ownerID = guild.ownerID;
+    client.settings.set(guild.id, server);
+  }
 }
