8f236e5eef3d7c1110824f6bf5f745f65c5a63e2
Hodoo <8972895+Hodoo@users.noreply.github.com>
2020-06-02 13:47:06 -0400

    added typeof test

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -6,6 +6,9 @@ exports.run = (client, message) => {
   // Get information from enmap
   let server = client.settings.get(message.guild.id);
 
+  console.log(typeof message.id);
+  console.log(typeof Object.keys(server.selfassigns)[0]);
+
   if (Object.keys(server.selfassigns).includes(message.id)) {
     delete server.selfassigns[message.id];
     console.log(Object.keys(server.selfassigns).toString()); // Testing line
