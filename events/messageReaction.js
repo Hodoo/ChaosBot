cba934f5ba438a0ddcc814dc3906bf6c047a3ab4
Hodoo <8972895+Hodoo@users.noreply.github.com>
2020-06-02 16:05:20 -0400

    Initial reaction code

--- events/messageReaction.js
+++ events/messageReaction.js
@@ -1,3 +1,24 @@
-exports.run = (client, reaction, user, state) => {
-  console.log(`Successfully fired in ${state}`)
+exports.run = (client, messageReaction, user, state) => {
+
+  // Ignore DMs
+  if (message.channel.type !=='text') return;
+
+  // Get information from enmap
+  let server = client.settings.get(messageReaction.message.guild.id);
+
+  if (!Object.keys(server.selfassigns).includes(messageReaction.message.id)) return;
+  if (!Object.keys(server.selfassigns[messageReaction.message.id]["assigns"]).includes(messageReaction.emoji.id)) return;
+
+  let member = messageReaction.message.guild.member(user);
+
+  if (state == "Add") {
+    if (member.roles.has(server.selfassigns[messageReaction.message.id]["assigns"][messageReaction.emoji.id])) return;
+    member.addRole(server.selfassigns[messageReaction.message.id]["assigns"][messageReaction.emoji.id]);
+  };
+
+  if (state == "Remove") {
+    if (member.roles.has(server.selfassigns[messageReaction.message.id]["assigns"][messageReaction.emoji.id])) {
+      member.removeRole(server.selfassigns[messageReaction.message.id]["assigns"][messageReaction.emoji.id]);
+    }
+  };
 }
