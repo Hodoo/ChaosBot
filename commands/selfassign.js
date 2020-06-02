1fa9f290dae043297a8c07de74d625c4a0533105
Hodoo <8972895+Hodoo@users.noreply.github.com>
2020-06-02 14:01:03 -0400

    fixed incorrect message id being recorded

--- commands/selfassign.js
+++ commands/selfassign.js
@@ -52,7 +52,7 @@ exports.run = (client, server, message, args) => {
       channel.send(newmsg).then(sentMsg => {
         reactions.forEach(reactEmote => sentMsg.react(reactEmote));
       })
-      server.selfassigns[message.id] = {"header":header, "roles":roles,"assigns":assigns,"footer":footer};
+      server.selfassigns[sentMsg.id] = {"header":header, "roles":roles,"assigns":assigns,"footer":footer};
       console.log(Object.keys(server.selfassigns).toString()); // Testing line
       client.settings.set(message.guild.id, server);
       return;
