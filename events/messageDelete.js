869d15bbee92244500ef6660b1ad4b1f0fe9e6a2
Hodoo <8972895+Hodoo@users.noreply.github.com>
2020-06-02 13:41:08 -0400

    switch to array.includes

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -6,8 +6,8 @@ exports.run = (client, message) => {
   // Get information from enmap
   let server = client.settings.get(message.guild.id);
 
-  if (server.selfassigns.hasOwnProperty(message.id.toString())) {
-    delete server.selfassigns[message.id];
+  if (Object.keys(server.selfassigns).includes(message.id.toString())) {
+    delete server.selfassigns[message.id.toString()];
     console.log(Object.keys(server.selfassigns).toString()); // Testing line
     client.settings.set(message.guild.id, server);
   };
