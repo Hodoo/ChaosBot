8196eff6d7699d09a159ac1350d54c8f628ff7d9
Hodoo <8972895+Hodoo@users.noreply.github.com>
2020-10-14 19:13:05 -0400

    Added partials support for reaction handling

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var app = express();
 const fs = require('fs');
 const Discord = require("discord.js");
 const config = require("./data/config.json");
-const client = new Discord.Client();
+const client = new Discord.Client({ partials: ['MESSAGE', 'REACTION'] });
 require("./functions/functions.js")(client);
 var pkginfo = require('pkginfo')(module, 'version');
 client.version = module.exports.version
@@ -49,37 +49,6 @@ client.on('guildMemberUpdate', (...args) => require(`./events/guildMemberUpdate.
 
 client.on('messageReactionAdd', (messageReaction, user) => require(`./events/messageReaction.js`).run(client, messageReaction, user, "Add"));
 client.on('messageReactionRemove', (messageReaction, user) => require(`./events/messageReaction.js`).run(client, messageReaction, user, "Remove"));
-
-client.on('raw', packet => {
-    // We don't want this to run on unrelated packets
-    if (['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) {
-      // Grab the channel to check the message from
-      const channel = client.channels.cache.get(packet.d.channel_id);
-      // There's no need to emit if the message is cached, because the event will fire anyway for that
-      if (channel.messages.cache.has(packet.d.message_id)) return;
-      // Since we have confirmed the message is not cached, let's fetch it
-      channel.messages.fetch(packet.d.message_id).then(message => {
-          // Emojis can have identifiers of name:id format, so we have to account for that case as well
-          const emoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name;
-          // This gives us the reaction we need to emit the event properly, in top of the message object
-          const messageReaction = message.reactions.cache.get(emoji);
-          // Adds the currently reacting user to the reaction's users collection.
-          if (messageReaction) messageReaction.users.set(packet.d.user_id, client.users.cache.get(packet.d.user_id));
-          // Check which type of event it is before emitting
-          if (packet.t === 'MESSAGE_REACTION_ADD') {
-              client.emit('messageReactionAdd', messageReaction, client.users.cache.get(packet.d.user_id));
-          }
-          if (packet.t === 'MESSAGE_REACTION_REMOVE') {
-              client.emit('messageReactionRemove', messageReaction, client.users.cache.get(packet.d.user_id));
-          }
-      });
-    } else if (packet.t === 'MESSAGE_DELETE') {
-      if (!packet.d.guild_id) return;
-      const message = {"id":packet.d.id,"guild":{"id":packet.d.guild_id},"channel":{"type":"text"}}
-      client.emit('messageDelete', message);
-    }
-});
-
 
 
 client.login(config.token);
--- events/messageReaction.js
+++ events/messageReaction.js
@@ -1,40 +1,48 @@
 exports.run = (client, messageReaction, user, state) => {
 
-  // Ignore DMs
-  if (messageReaction.message.channel.type !=='text') return;
-  // Ignore Bots
-  if (user.bot) return;
+  async function reactionHandler() {
+    // Fetch full reaction if partial
+    if (messageReaction.partial) {
+      messageReaction = await messageReaction.fetch();
+    };
+    // Ignore DMs
+    if (messageReaction.message.channel.type !=='text') return;
+    // Ignore Bots
+    if (user.bot) return;
 
-  // Get information from enmap
-  let server = client.settings.get(messageReaction.message.guild.id);
+    // Get information from enmap
+    let server = client.settings.get(messageReaction.message.guild.id);
 
-  if (!Object.keys(server.selfassigns).includes(messageReaction.message.id)) return;
-  if (!Object.keys(server.selfassigns[messageReaction.message.id]["assigns"]).includes(messageReaction.emoji.id)) return;
+    if (!Object.keys(server.selfassigns).includes(messageReaction.message.id)) return;
+    if (!Object.keys(server.selfassigns[messageReaction.message.id]["assigns"]).includes(messageReaction.emoji.id)) return;
 
-  let member = messageReaction.message.guild.member(user);
-  let reqroles = server.selfassigns[messageReaction.message.id]["roles"];
-  let allowed = 0;
+    let member = messageReaction.message.guild.member(user);
+    let reqroles = server.selfassigns[messageReaction.message.id]["roles"];
+    let allowed = 0;
 
-  if (reqroles.length > 0) {
-    reqroles.forEach(element => {
-      if (member.roles.cache.has(element)) {allowed = 1}});
-  } else {allowed = 1};
+    if (reqroles.length > 0) {
+      reqroles.forEach(element => {
+        if (member.roles.cache.has(element)) {allowed = 1}});
+    } else {allowed = 1};
 
 
-  if (state == "Add") {
-    if (allowed == 1) {
-      if (member.roles.cache.has(server.selfassigns[messageReaction.message.id]["assigns"][messageReaction.emoji.id])) return;
-      member.roles.add(server.selfassigns[messageReaction.message.id]["assigns"][messageReaction.emoji.id]);
-    } else {
-      messageReaction.remove(user);
-    }
-  };
-
-  if (state == "Remove") {
-    if (allowed == 1) {
-      if (member.roles.cache.has(server.selfassigns[messageReaction.message.id]["assigns"][messageReaction.emoji.id])) {
-        member.roles.remove(server.selfassigns[messageReaction.message.id]["assigns"][messageReaction.emoji.id]);
+    if (state == "Add") {
+      if (allowed == 1) {
+        if (member.roles.cache.has(server.selfassigns[messageReaction.message.id]["assigns"][messageReaction.emoji.id])) return;
+        member.roles.add(server.selfassigns[messageReaction.message.id]["assigns"][messageReaction.emoji.id]);
+      } else {
+        messageReaction.remove(user);
       }
-    }
+    };
+
+    if (state == "Remove") {
+      if (allowed == 1) {
+        if (member.roles.cache.has(server.selfassigns[messageReaction.message.id]["assigns"][messageReaction.emoji.id])) {
+          member.roles.remove(server.selfassigns[messageReaction.message.id]["assigns"][messageReaction.emoji.id]);
+        }
+      }
+    };
   };
+  reactionHandler();
+
 }
